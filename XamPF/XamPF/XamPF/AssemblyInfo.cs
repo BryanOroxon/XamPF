bc06432d185b7f5a217fe7c336b3e780cdbb4ba2
Daniel Angel Monettelli L <danielmonetelli@hotmail.com>
2021-06-05 12:30:47 -0500

    Added Style flow in XamPF Part1.
    1.- Added Embedded Fonts flow:
          - customfonticons.ttf.
          - Font Family - Montserrat.
    2.- Resource flow in App.xaml:
           - Colors.
           - FontImageSource Tabs.
           - FontImageSource Interfaces.
           - Text Size.
    3.- The "AudioBookPage.xaml" file was added in its respective folder in "test mode for fonts".
    4.- The "Styles.xaml" file has been added in its respective folder.

--- XamPF/XamPF/XamPF/AssemblyInfo.cs
+++ XamPF/XamPF/XamPF/AssemblyInfo.cs
@@ -1,3 +1,33 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+
+#region FontIcons Embedded
+// CUSTOM FONT ICONS WITH "IconMoon" and "Fontello"
+[assembly: ExportFont("customfonticons.ttf", Alias = "CustomFontIcons")]
+#endregion
+
+#region FontFamily Embedded 
+#region FONTFAMILY [Montserrat]
+[assembly: ExportFont("Montserrat-Black.ttf", Alias = "Montserrat_Black")]
+[assembly: ExportFont("Montserrat-BlackItalic.ttf", Alias = "Montserrat_BlackItalic")]
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "Montserrat_Bold")]
+[assembly: ExportFont("Montserrat-BoldItalic.ttf", Alias = "Montserrat_BoldItalic")]
+[assembly: ExportFont("Montserrat-ExtraBold.ttf", Alias = "Montserrat_ExtraBold")]
+[assembly: ExportFont("Montserrat-ExtraBoldItalic.ttf", Alias = "Montserrat_ExtraBoldItalic")]
+[assembly: ExportFont("Montserrat-ExtraLight.ttf", Alias = "Montserrat_ExtraLight")]
+[assembly: ExportFont("Montserrat-ExtraLightItalic.ttf", Alias = "Montserrat_ExtraLightItalic")]
+[assembly: ExportFont("Montserrat-Italic.ttf", Alias = "Montserrat_Italic")]
+[assembly: ExportFont("Montserrat-Light.ttf", Alias = "Montserrat_Light")]
+[assembly: ExportFont("Montserrat_LightItalic.ttf", Alias = "Montserrat_LightItalic")]
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Montserrat_Medium")]
+[assembly: ExportFont("Montserrat-MediumItalic.ttf", Alias = "Montserrat_MediumItalic")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat_Regular")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Montserrat_SemiBold")]
+[assembly: ExportFont("Montserrat-SemiBoldItalic.ttf", Alias = "Montserrat_SemiBoldItalic")]
+[assembly: ExportFont("Montserrat-Thin.ttf", Alias = "Montserrat_Thin")]
+[assembly: ExportFont("Montserrat-ThinItalic.ttf", Alias = "Montserrat_ThinItalic")]
+#endregion
+#endregion
--- XamPF/XamPF/XamPF/Views/Books/Styles.xaml.cs
+++ XamPF/XamPF/XamPF/Views/Books/Styles.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace XamPF
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamPF.Views.Books
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Styles : ResourceDictionary
     {
-        public MainPage()
+        public Styles()
         {
             InitializeComponent();
         }
